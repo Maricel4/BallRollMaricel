2ebe3e33c0e2c58bf64fd7ff64fa70843a1f7584
Maricel4 <42561008+Maricel4@users.noreply.github.com>
2020-01-15 15:43:35 -0600

    teehee ball roll
    :)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+//this creates an editable property in our inspector
+  public float speed;
+//creates a variable to hold the reference
+  public Text countText;
+  public Text winText;
+  private Rigidbody rb;
+  private int count;
+  void Start()
+{
+  rb = GetComponent<Rigidbody>();
+  count = 0;
+  SetCountText ();
+  winText.text = "";
+}
+
+  void FixedUpdate()
+{
+  //This is where the physics code will go.
+  float moveHorizontal = Input.GetAxis ("Horizontal");
+  float moveVertical = Input.GetAxis ("Vertical");
+// x , y , z values determine force we will add to ball. x is moveHorizontal, z is moveVertical
+  Vector3 movement = new Vector3 ( moveHorizontal, 0.0f, moveVertical);
+
+  rb.AddForce (movement * speed);
+}
+  void OnTriggerEnter (Collider other)
+  {
+    if(other.gameObject.CompareTag("Pick Up"))
+    {
+      other.gameObject.SetActive(false);
+      count = count + 1;
+      SetCountText ();
+    }
+  }
+  void SetCountText ()
+  {
+    countText.text = "Count: " + count.ToString ();
+      if(count >= 13)
+      {
+        winText.text = "You Win!";
+      }
+  }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour{
+  // Update is called once per frame
+    void Update()
+    {
+      //the multiply makes it smooth and framerate independent
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+    }
+}
