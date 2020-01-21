971ac9a4a9c751a682c8ece6d63c5df42b0b4e57
Chris B <officialchrisb@icloud.com>
2020-01-21 15:28:20 -0600

    Added sound effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,26 +9,30 @@ public class PlayerController : MonoBehaviour {
   public Text countText;
   public Text winText;
   public int goal;
+  AudioSource audio;
+  public AudioClip pickupSound;
+  public AudioClip winSound;
   private Rigidbody rb;
   private int count;
   void Start()
-{
-  rb = GetComponent<Rigidbody>();
-  count = 0;
-  SetCountText ();
-  winText.text = "";
-}
+  {
+    rb = GetComponent<Rigidbody>();
+    count = 0;
+    SetCountText ();
+    winText.text = "";
+    audio = GetComponent<AudioSource>();
+  }
 
   void FixedUpdate()
-{
-  //This is where the physics code will go.
-  float moveHorizontal = Input.GetAxis ("Horizontal");
-  float moveVertical = Input.GetAxis ("Vertical");
-// x , y , z values determine force we will add to ball. x is moveHorizontal, z is moveVertical
-  Vector3 movement = new Vector3 ( moveHorizontal, 0.0f, moveVertical);
+  {
+    //This is where the physics code will go.
+    float moveHorizontal = Input.GetAxis ("Horizontal");
+    float moveVertical = Input.GetAxis ("Vertical");
+  // x , y , z values determine force we will add to ball. x is moveHorizontal, z is moveVertical
+    Vector3 movement = new Vector3 ( moveHorizontal, 0.0f, moveVertical);
 
-  rb.AddForce (movement * speed);
-}
+    rb.AddForce (movement * speed);
+  }
   void OnTriggerEnter (Collider other)
   {
     if(other.gameObject.CompareTag("Pick Up"))
@@ -36,6 +40,7 @@ public class PlayerController : MonoBehaviour {
       other.gameObject.SetActive(false);
       count = count + 1;
       SetCountText ();
+      audio.PlayOneShot(pickupSound);
     }
   }
   void SetCountText ()
@@ -44,6 +49,7 @@ public class PlayerController : MonoBehaviour {
       if(count >= goal)
       {
         winText.text = "You Win!";
+        audio.PlayOneShot(winSound);
       }
   }
 }
